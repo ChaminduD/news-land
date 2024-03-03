978219c98c2595cf887ae1e2ba735e6d4e6f7c64
ChaminduD <chamindudulanjana10@gmail.com>
2024-03-03 13:17:07 +0530

    Add categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from "react"
 import Navbar from "./Components/Navbar"
 import NewsBoard from "./Components/NewsBoard"
 
 function App() {
+  const [category, setCategory] = useState("general");
   return (
     <>
-    <Navbar/>
-    <NewsBoard/>
+    <Navbar setCategory={setCategory}/>
+    <NewsBoard category={category}/>
     </>
   )
 }
