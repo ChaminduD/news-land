16a56350846b9c41cc4720884b2c68c53cdff3a6
ChaminduD <chamindudulanjana10@gmail.com>
2024-03-03 12:41:59 +0530

    Add props to NewsItem

--- src/Components/NewsBoard.jsx
+++ src/Components/NewsBoard.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react"
+import NewsItem from "./NewsItem";
 
 
 const NewsBoard = () => {
@@ -15,6 +16,9 @@ const NewsBoard = () => {
     return (
         <div>
             <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+            {articles.map((news, index) => {
+                return <NewsItem key={index} title={news.title} description={news.description} src={news.urlToImage} url={news.url}/>
+            })}
         </div>
     )
 }
