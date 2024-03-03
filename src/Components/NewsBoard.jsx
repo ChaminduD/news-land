09b406bb37c4195d2122e9ae44f5829f69e99569
ChaminduD <chamindudulanjana10@gmail.com>
2024-03-03 12:04:21 +0530

    Add Components folder

---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,10 @@
+
+
+const NewsBoard = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default NewsBoard
---
+++ src/Components/NewsItem.jsx
@@ -0,0 +1,10 @@
+
+
+const NewsItem = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default NewsItem
